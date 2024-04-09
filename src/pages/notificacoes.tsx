9d9a5667b16ee13733be9d5f9dee59e8c4a08f47
adcavalcant <aderc19@gmail.com>
2024-04-09 12:04:15 -0300

    :sparkles: feat: Utilização do Context API para compartilhamento de dados entre os componentes da aplicação.

--- src/pages/notificacoes.tsx
+++ src/pages/notificacoes.tsx
@@ -1,9 +1,13 @@
 import Layout from "../components/template/Layout";
+import { AppConsumer } from "../data/context/AppContext";
+import useAppData from "../data/hook/useAppData";
 
 export default function Notificacoes() {
+  const contexto = useAppData();
+
   return (
     <Layout titulo="Notificações" subtitulo="Aqui você irá gerenciar as suas notificações!">
-      <h1>Conteúdo</h1>
+      <h3>{contexto.nome}</h3>
     </Layout>
   );
 }
