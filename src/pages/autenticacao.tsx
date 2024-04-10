2beaa6913dae4724a7abb14896715165d7ff5737
3S Cavalcante <aderc19@gmail.com>
2024-04-10 10:21:28 -0300

    :construction: Estilização da página de autenticação.

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,17 +1,45 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
+import { IconeGoogle } from "../components/icons";
 
 export default function Autenticacao() {
-  const [modo, setModo] = useState<'login' | 'cadastro'>('login');
+  const [modo, setModo] = useState<"login" | "cadastro">("login");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
+  function submeter() {
+    if (modo === "login") {
+      console.log("login");
+    } else {
+      console.log("cadastro");
+    }
+  }
+
   return (
-    <div>
-      <h1>Autenticacao</h1>
-      <AuthInput label="Email" tipo="email" valor={email} valorMudou={setEmail} obrigatorio />
-      <AuthInput label="Senha" tipo="password" valor={senha} valorMudou={setSenha} />
-      {/* <AuthInput label="Confirmação de Senha" tipo="password" valor={senha} valorMudou={setSenha} naoRenderizarQuando={modo === 'login'} /> */}
+    <div className={`flex flex-col h-screen items-center justify-center`}>
+      <div className={`w-1/2`}>
+        <h1 className={`text-xl font-bold mb-5`}>
+          {modo === "login" ? "Entre com a Sua Conta" : "Cadastre-se na Plataforma"}
+        </h1>
+        <AuthInput label="Email" tipo="email" valor={email} valorMudou={setEmail} obrigatorio />
+        <AuthInput label="Senha" tipo="password" valor={senha} valorMudou={setSenha} />
+        {/* <AuthInput label="Confirmação de Senha" tipo="password" valor={senha} valorMudou={setSenha} naoRenderizarQuando={modo === 'login'} /> */}
+
+        <button
+          onClick={submeter}
+          className={`w-full bg-indigo-500 hover:bg-indigo-400 text-white rounded-lg px-4 py-3 mt-6`}
+        >
+          {modo === "login" ? "Entrar" : "Cadastrar"}
+        </button>
+        <hr className="my-6 border-gray-300 w-full" />
+        <button
+          onClick={submeter}
+          className={`flex items-center justify-center gap-3 w-full bg-red-500 hover:bg-red-400 text-white rounded-lg px-4 py-3`}
+        >
+          <span className="bg-white rounded-full p-1/2 flex items-center justify-center">{IconeGoogle}</span>
+          <span className="flex-grow">Entrar com Google</span>
+        </button>
+      </div>
     </div>
   );
 }
