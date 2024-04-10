2172d3d64dadfafba399d48ce125c3cfbb7aef6a
3S Cavalcante <aderc19@gmail.com>
2024-04-10 16:35:34 -0300

    :sparkles: feat: Adicionando imagem do usuário google.

--- src/components/template/Cabecalho.tsx
+++ src/components/template/Cabecalho.tsx
@@ -1,6 +1,7 @@
 import Titulo from "./Titulo";
 import BotaoAlternarTema from "./BotaoAlternarTema";
 import useAppData from "../../data/hook/useAppData";
+import AvatarUsuario from "./AvatarUsuario";
 
 interface CabecalhoProps {
   titulo: string;
@@ -13,8 +14,9 @@ export default function Cabecalho(props: CabecalhoProps) {
   return (
     <div className={"flex"}>
       <Titulo titulo={props.titulo} subtitulo={props.subtitulo} />
-      <div className={"flex flex-grow justify-end"}>
+      <div className={"flex flex-grow justify-end items-center"}>
         <BotaoAlternarTema tema={tema} alternarTema={alternarTema}></BotaoAlternarTema>
+        <AvatarUsuario className="ml-3" />
       </div>
     </div>
   );
