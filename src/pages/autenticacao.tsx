93684054ea0e3c221276040965b82312eee0c0e5
3S Cavalcante <aderc19@gmail.com>
2024-04-10 09:58:56 -0300

    :construction: Implementando Página de Autenticação.

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,7 +1,17 @@
+import { useState } from "react";
+import AuthInput from "../components/auth/AuthInput";
+
 export default function Autenticacao() {
+  const [modo, setModo] = useState<'login' | 'cadastro'>('login');
+  const [email, setEmail] = useState("");
+  const [senha, setSenha] = useState("");
+
   return (
     <div>
       <h1>Autenticacao</h1>
+      <AuthInput label="Email" tipo="email" valor={email} valorMudou={setEmail} obrigatorio />
+      <AuthInput label="Senha" tipo="password" valor={senha} valorMudou={setSenha} />
+      {/* <AuthInput label="Confirmação de Senha" tipo="password" valor={senha} valorMudou={setSenha} naoRenderizarQuando={modo === 'login'} /> */}
     </div>
   );
 }
