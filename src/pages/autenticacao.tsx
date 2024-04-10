41c0ad353c1685c8e558115bd437bb7f5624e03a
3S Cavalcante <aderc19@gmail.com>
2024-04-10 11:17:13 -0300

    :lipstick: feat: Adicionando mensagem de erro na tela de login e cadastro.

--- src/pages/autenticacao.tsx
+++ src/pages/autenticacao.tsx
@@ -1,17 +1,25 @@
 import { useState } from "react";
 import AuthInput from "../components/auth/AuthInput";
-import { IconeGoogle } from "../components/icons";
+import { IconeAtencao, IconeGoogle } from "../components/icons";
 
 export default function Autenticacao() {
+  const [erro, setErro] = useState(null);
   const [modo, setModo] = useState<"login" | "cadastro">("login");
   const [email, setEmail] = useState("");
   const [senha, setSenha] = useState("");
 
+  function exibirErro(msg, tempoEmSegundos = 5) {
+    setErro(msg);
+    setTimeout(() => setErro(null), tempoEmSegundos * 1000);
+  }
+
   function submeter() {
     if (modo === "login") {
       console.log("login");
+      exibirErro('Ocorreu um erro no login!')
     } else {
       console.log("cadastro");
+      exibirErro('Ocorreu um erro no cadastro!')
     }
   }
 
@@ -28,6 +36,18 @@ export default function Autenticacao() {
         <h1 className={`text-3xl font-bold mb-5`}>
           {modo === "login" ? "Entre com a Sua Conta" : "Cadastre-se na Plataforma"}
         </h1>
+        {erro ? (
+          <div
+            className={`flex items-center
+        bg-red-400 text-white py-3 px-5 my-2 border-2 border-red-700 rounded-lg`}
+          >
+            {IconeAtencao()}
+            <span className="ml-3 text-sm">{erro}</span>
+          </div>
+        ) : (
+          false
+        )}
+
         <AuthInput label="Email" tipo="email" valor={email} valorMudou={setEmail} obrigatorio />
         <AuthInput label="Senha" tipo="password" valor={senha} valorMudou={setSenha} />
         {/* <AuthInput label="Confirmação de Senha" tipo="password" valor={senha} valorMudou={setSenha} naoRenderizarQuando={modo === 'login'} /> */}
@@ -52,7 +72,9 @@ export default function Autenticacao() {
             <a
               onClick={() => setModo("cadastro")}
               className={`text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}
-            > Crie uma conta gratuitamente
+            >
+              {" "}
+              Crie uma conta gratuitamente
             </a>{" "}
           </p>
         ) : (
@@ -61,7 +83,9 @@ export default function Autenticacao() {
             <a
               onClick={() => setModo("login")}
               className={`text-blue-500 hover:text-blue-700 font-semibold cursor-pointer`}
-            > Entre com as suas credenciais
+            >
+              {" "}
+              Entre com as suas credenciais
             </a>{" "}
           </p>
         )}
