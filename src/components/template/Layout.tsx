1e827f6249d59d1dcf6acdee1a18dd7039eb4444
3S Cavalcante <aderc19@gmail.com>
2024-04-12 10:34:46 -0300

    :sparkles: feat: Modificando chamada do 'Forçar Autenticação' para uma função.

--- src/components/template/Layout.tsx
+++ src/components/template/Layout.tsx
@@ -2,7 +2,8 @@ import MenuLateral from "./MenuLateral";
 import Cabecalho from "./Cabecalho";
 import Conteudo from "./Conteudo";
 import useAppData from "../../data/hook/useAppData";
-import ForcarAutenticacao from "../auth/ForcarAutenticacao";
+import forcarAutenticacao from "../../functions/ForcarAutenticacao";
+// import ForcarAutenticacao from "../auth/ForcarAutenticacao";
 
 interface LayoutProps {
   titulo: string;
@@ -12,16 +13,13 @@ interface LayoutProps {
 
 export default function Layout(props: LayoutProps) {
   const { tema } = useAppData();
-
-  return (
-    <ForcarAutenticacao>
-      <div className={`${tema} flex h-screen w-screen`}>
-        <MenuLateral />
-        <div className="flex flex-col w-full bg-gray-300 p-7 dark:bg-gray-800">
-          <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
-          <Conteudo>{props.children}</Conteudo>
-        </div>
+  return forcarAutenticacao(
+    <div className={`${tema} flex h-screen w-screen`}>
+      <MenuLateral />
+      <div className="flex flex-col w-full bg-gray-300 p-7 dark:bg-gray-800">
+        <Cabecalho titulo={props.titulo} subtitulo={props.subtitulo} />
+        <Conteudo>{props.children}</Conteudo>
       </div>
-    </ForcarAutenticacao>
+    </div>
   );
 }
